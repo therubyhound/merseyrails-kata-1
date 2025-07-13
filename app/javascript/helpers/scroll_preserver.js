d12afa12a45d0f8ff9f3f9e4a247542be50ed9ca
Steven Harrison <ste@hey.com>
2025-07-13 10:31:30 +0000

    Prevent redundant queries

--- app/javascript/helpers/scroll_preserver.js
+++ app/javascript/helpers/scroll_preserver.js
@@ -2,13 +2,13 @@ function preserveScrollPositions() {
   var scrollPositions = {}
 
   addEventListener("turbo:before-render", function(){
-    document.querySelectorAll("[data-turbo-permanent]").forEach(function(element){ 
+    document.querySelectorAll("[data-preserve-scroll]").forEach(function(element){ 
       scrollPositions[element.id] = element.scrollTop
     })
   })
 
   addEventListener("turbo:render", function(){
-    document.querySelectorAll("[data-turbo-permanent]").forEach(function(element){ 
+    document.querySelectorAll("[data-preserve-scroll]").forEach(function(element){ 
       element.scrollTop = scrollPositions[element.id]
     })
   })
