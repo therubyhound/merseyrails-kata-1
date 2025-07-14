454025835c0d163950d48967fc5bfe9f07d1b854
Steven Harrison <ste@hey.com>
2025-07-14 21:50:23 +0000

    Scroll to active selection

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -4,19 +4,34 @@ export default class extends Controller {
   static classes = [ "active" ]
   static values = { selected: String }
 
+  connect() {
+    if (!this.hasSelectedValue) return
+    const selection = this.element.querySelector(`#${this.selectedValue}`)
+    if (!selection) return
+    this.#scrollToSelection(selection)
+    this.#setActiveSelection(selection)
+  }
+
   select(event) {
     this.#clearPreviousSelection()
     this.#setActiveSelection(event.currentTarget)
+  }
+
+  #scrollToSelection(target) {
+    const stickyHeader = this.element.querySelector('.sticky')
+    const headerHeight = stickyHeader?.offsetHeight ?? 0
+    const elementTop = target.offsetTop - headerHeight
+    this.element.scrollTo({ top: elementTop, behavior: "auto" })
+  }
+
+  #setActiveSelection(target) {
+    this.selectedValue = target?.id
+    target.classList.add(this.activeClass)
   }
 
   #clearPreviousSelection() {
     if (!this.hasSelectedValue) return
     const previousSelection = this.element.querySelector(`#${this.selectedValue}`)
     previousSelection?.classList.remove(this.activeClass)
-  }
-
-  #setActiveSelection(target) {
-    this.selectedValue = target.id
-    target.classList.add(this.activeClass)
   }
 }
